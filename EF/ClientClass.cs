f4b98a43a9040ea391365a73bbdf0b820a519180
Артём <artem.zeynetdinov@mail.ru>
2022-04-28 10:37:18 +0300

    Добавьте файлы проекта.

---
+++ EF/ClientClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Language_3ISP97_TuzhilovDvoryaninov.EF
+{
+    class ClientClass
+    {
+    }
+}
